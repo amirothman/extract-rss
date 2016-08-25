ed16c848990265810fc7dbcb8f30573d5c97c990
Amir Othman <th3go2@gmail.com>
2016-08-25 23:40:59 +0200

    :)

---
+++ filter_news_links.py
@@ -0,0 +1,41 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+
+with open("news_links.txt","r") as links:
+    with open("filtered.txt","w") as filtered:
+        for line in links:
+            if re.search(r"^http",line):
+                c = re.sub("\n","",line)
+                if not re.search(r"facebook|twitter|wikipedia|htm",c):
+                    # print(c)
+                    try:
+                        r = requests.get(c)
+                        if re.search(r"rss",r.text):
+                            soup = BeautifulSoup(r.text,"html.parser")
+                            rss = soup.findAll('a',href=True)
+                            # print(re.search(r"rss",r.text))
+                            # print(c)
+                            for s in rss:
+                                if re.search(r"rss",s["href"]):
+                                    if re.search(r"^http",s["href"]):
+                                        real_rss = s["href"]
+                                        real_rss = re.sub("//","/",real_rss)
+                                        real_rss = re.sub("http:/","http://",real_rss)
+                                        real_rss = re.sub("https:/","https://",real_rss)
+                                    else:
+                                        if re.search(r"/$",c):
+                                            real_rss = re.sub(" ","","".join([c,s["href"]]))
+                                        else:
+                                            real_rss = re.sub(" ","","/".join([c,s["href"]]))
+                                        real_rss = re.sub("//","/",real_rss)
+                                        real_rss = re.sub("http:/","http://",real_rss)
+                                        real_rss = re.sub("https:/","https://",real_rss)
+
+                                    print(real_rss)
+                                    filtered.write(real_rss)
+                                    filtered.write("\n")
+                    except requests.exceptions.ConnectionError:
+                        print("requests.exceptions.ConnectionError")
+                    except requests.exceptions.TooManyRedirects:
+                        print("requests.exceptions.TooManyRedirects")
---
+++ sedut_pakai_selenium.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+import time
+from bs4 import BeautifulSoup
+from readability.readability import Document
+import re
+
+def return_links(html):
+    soup = BeautifulSoup(html, 'html.parser')
+
+    return[ l["href"] for l in soup.find_all("a", href=True)]
+
+if __name__=="__main__":
+
+    driver = webdriver.Chrome("./chromedriver")
+    # links_cleared = []
+    countries = []
+    with open("countries","r") as country:
+        for line in country:
+            countries.append(re.sub(r"\n","",line).lower())
+    base_link = "https://www.google.de/#q={}+english+news{}"
+    pages = ["","&start=10","&start=20"]
+    wait = WebDriverWait(driver, 10)
+
+    with open("news_links.txt","w") as newslinks_txt:
+        for country in countries:
+            for p in pages:
+                url = base_link.format(country,p)
+                # print(link)
+
+                driver.get(url)
+                # wait.until(EC.text_to_be_present_in_element((By.ID,'resultStats'),"Sekunden"))
+                time.sleep(4)
+
+                links = return_links(driver.page_source)
+
+                # links = [f.get_attribute("href") for f in driver.find_elements_by_tag_name("a")]
+                for link in links:
+                    if link and not re.search(r"google|youtube|javascript|blogger",str(link)):
+                        print(link)
+                        # links_cleared.append(link)
+                        newslinks_txt.write(link)
+                        newslinks_txt.write("\n")
+    driver.quit()
